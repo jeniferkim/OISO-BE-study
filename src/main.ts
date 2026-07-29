5f4f166be5ff469f71866c38df9555a4f77f7b2b
jeniferkim <jeniferkim1673@naver.com>
2026-07-29 19:41:15 +0900

    feat: 전역 ValidationPipe 등록

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, // DTO에 정의되지 않은 필드 제거
+      transform: true, // 요청값을 DTO 타입이나 파라미터 타입에 맞게 변환하는 기능을 활성화
+    }),
+  );
+
   await app.listen(process.env.PORT ?? 3000);
 }
+
 bootstrap();
