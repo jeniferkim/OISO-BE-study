15551d6e7b510318c1505fbe00e8253605b6597c
jeniferkim <jeniferkim1673@naver.com>
2026-08-26 21:49:43 +0900

    feat: 저장 루트 생성 트랜잭션 적용 및 롤백 테스트 주석 처리

--- src/saved-route/saved-route.service.ts
+++ src/saved-route/saved-route.service.ts
@@ -4,6 +4,7 @@ import {
   NotFoundException,
 } from '@nestjs/common';
 
+import { PrismaService } from '../prisma/prisma.service';
 import { CreateSavedRouteDto } from './dto/create-saved-route.dto';
 import { SavedRouteRepository } from './saved-route.repository';
 import type {
@@ -14,7 +15,10 @@ import type {
 
 @Injectable()
 export class SavedRouteService {
-  constructor(private readonly savedRouteRepository: SavedRouteRepository) {}
+  constructor(
+    private readonly savedRouteRepository: SavedRouteRepository,
+    private readonly prisma: PrismaService,
+  ) {}
 
   async findAll(): Promise<SavedRouteListResponse> {
     const savedRouteRecords = await this.savedRouteRepository.findAll();
@@ -70,15 +74,33 @@ export class SavedRouteService {
     userId: number,
     createSavedRouteDto: CreateSavedRouteDto,
   ): Promise<SavedRoute> {
-    const savedRoute = await this.savedRouteRepository.create(
-      createSavedRouteDto.title,
-      createSavedRouteDto.savingAmount,
-      userId,
-    );
+    const savedRoute = await this.prisma.$transaction(async (tx) => {
+      const route = await tx.savedRoute.create({
+        data: {
+          title: createSavedRouteDto.title,
+          savingAmount: createSavedRouteDto.savingAmount,
+          userId,
+        },
+      });
+
+      // throw new Error('롤백 테스트'); // 서버 재실행 해야함
+
+      await tx.user.update({
+        where: {
+          id: userId,
+        },
+        data: {
+          savedRouteCount: {
+            increment: 1,
+          },
+        },
+      });
+
+      return route;
+    });
 
     return this.toResponse(savedRoute);
   }
-
   async remove(
     userId: number,
     savedRouteId: number,
